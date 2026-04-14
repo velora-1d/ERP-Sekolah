534c05925bef2d94b3063aaf0223c5381eef510c
Mahin Utsman Nawawi, S.H <mahinutsmannawawi20@gmail.com>
2026-04-14 15:06:37 +0700

    fix: refine next.config.ts for ide compatibility and push

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   images: {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [
